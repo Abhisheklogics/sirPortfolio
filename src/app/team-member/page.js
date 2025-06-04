4e20b1a73de872c68322a0847c88f6dbfdd0c5cd
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-06-05 00:18:39 +0530

    team margittop change

--- src/app/team-member/page.js
+++ src/app/team-member/page.js
@@ -5,7 +5,7 @@ export default function OurTeam()
 {
     return(
     <>
-    <div className="bg-white py-24 md:mt-[-100px] sm:py-32">
+    <div className="bg-white py-24  sm:py-32">
   <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
     <div className="max-w-xl">
       <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">Meet our leadership</h2>
