255e737da102bb2ba1ba9f24988b4a24adf4e1b7
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-06-17 09:11:38 +0530

    Update Gallery.js

--- src/components/NewCom/Gallery.js
+++ src/components/NewCom/Gallery.js
@@ -182,7 +182,7 @@ export default function ProjectGallery() {
         </div>
 
       {zoomData.src && (
-  <div className="fixed inset-0 z-[9999] bg-black/90 backdrop-blur-sm flex items-center justify-center">
+  <div className="fixed inset-0 z-[9999] bg-black/90 backdrop-blur-sm ">
     {/* Close Button */}
     <button
       onClick={closeZoom}
