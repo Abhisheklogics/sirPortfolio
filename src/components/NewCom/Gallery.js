e29b03852f6ec2b7d1ede4ae7abb26cc8a168b17
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-06-17 10:07:52 +0530

    Update Gallery.js

--- src/components/NewCom/Gallery.js
+++ src/components/NewCom/Gallery.js
@@ -214,7 +214,7 @@ export default function ProjectGallery() {
       </div>
 
       {/* Arrows below image */}
-      <div className="flex justify-center items-center gap-8 h-[80px] mt-4">
+      <div className=" gap-8 h-[80px] ">
         <button
           onClick={() => changeImage(-1)}
           className="text-white text-3xl sm:text-4xl bg-black/50 hover:bg-black p-2 sm:p-3 rounded-full z-[10000] transition"
